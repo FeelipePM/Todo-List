dfe7e95088ceefe9cb2a773bd9577f17037de175
Felipe <Feelipe@email.com>
2018-10-22 21:48:39 -0300

    novas atualizações, atualizei a taskboard

---
+++ cadastro/cadastro.php
@@ -0,0 +1,8 @@
+<?php include_once '../top.php'?>
+            <h3 class="login">Cadastro</h3>
+            <input type="text" id="user" placeholder="Usuário" required>
+            <input type="email" class="form-control" id="email" placeholder="E-mail" required>
+            <input type="password" class="form-control" id="senha" placeholder="Senha" required>
+            <button type="submit" id="login">Cadastrar</button>
+            <a class="recuperar-senha" href="#">Esqueceu sua senha?</a>
+<?php include_once '../bottom.php'?>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-
-<?php include_once('topIndex.php');?>    
-  <form class="login-usuario" method="post" action="taskboard.php">
+<?php include_once 'top.php'?>    
+  <form class="login-usuario" method="post" action="taskboard/taskboard.php">
     <h3 class="login">Login</h3>
     <input type="text" id="user" placeholder="Usuário" required>
-    <input type="password" class="form-control" id="senha" placeholder="Senha" required>
-    <button type="submit" class="btn btn-default" id="login">Login</button>
+    <input type="password" id="senha" placeholder="Senha" required>
+    <button type="submit" id="login">Login</button>
   </form>
-  <form class="login-usuario" action="cadastro.php">
+  <form class="login-usuario" action="cadastro/cadastro.php">
     <button type="submit" id="criar-cadastro">Cadastre-se</button>
-    <a class="recuperar-senha" href="recuperar.php">Esqueceu sua senha?</a>
+    <a class="recuperar-senha" href="recuperar/recuperar.php">Esqueceu sua senha?</a>
   </form>
-<?php include_once('rodapeIndex.php');?>
+<?php include_once 'bottom.php'?>
